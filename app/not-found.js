71067e4d06103e5d0d2c714a7fe4f15694623dad
Charley Chen <charleychen2006@gmail.com>
2024-03-23 23:41:07 +0100

    Fixed 404 error page bug

--- app/not-found.js
+++ app/not-found.js
@@ -1,14 +1,10 @@
-"use client"
-
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 
 export default function NotFound() {
-  const router = useRouter()
   return (
     <div className="flex flex-col items-center p-2 mt-10 lg:m-0 lg:justify-center lg:h-screen">
       <h1 className="text-5xl font-bold">404</h1>
-      <p className="mt-4">{window.location.toString()}</p>
       <h2 className="text-3xl text-center font-bold my-10">Deze pagina kan niet worden gevonden</h2>
       <p className="text-lg mb-4">Navigeer snel naar</p>
       <div className="flex flex-wrap justify-center text-white gap-3">
