f791b1ea9d2e91b412d68e7a10a5f9de9c3c83c7
Charley Chen <charleychen2006@gmail.com>
2024-03-23 23:55:18 +0100

    Style content

--- app/(main)/contact/page.js
+++ app/(main)/contact/page.js
@@ -8,7 +8,7 @@ export default function Page() {
   return (
     <div className="flex flex-col items-center text-center py-10 px-4">
       <h1 className="text-3xl font-bold">Contact</h1>
-      <p className="mt-6 w-120">Heb je vragen, opmerkingen of suggesties? Neem gerust contact met ons op! Ons vriendelijke team staat klaar om je te helpen en al je vragen te beantwoorden. Of je nu wilt reserveren voor een speciale gelegenheid of meer wilt weten over onze menu-opties, wij staan voor je klaar. Kom langs bij Restaria Liba of neem telefonisch of via e-mail contact met ons op. We kijken ernaar uit om van je te horen!</p>
+      <p className="mt-6 max-w-120">Heb je vragen, opmerkingen of suggesties? Neem gerust contact met ons op! Ons vriendelijke team staat klaar om je te helpen en al je vragen te beantwoorden. Of je nu wilt reserveren voor een speciale gelegenheid of meer wilt weten over onze menu-opties, wij staan voor je klaar. Kom langs bij Restaria Liba of neem telefonisch of via e-mail contact met ons op. We kijken ernaar uit om van je te horen!</p>
       <p className="my-6 text-xl italic">Deze website is momenteel in onderhoud</p>
       <p>Website gemaakt door <a href="https://instagram.com/charleychen18" className="underline text-blue-800">Charley</a></p>
     </div>
--- app/(main)/restaria/page.js
+++ app/(main)/restaria/page.js
@@ -7,7 +7,7 @@ export default function Page() {
   return (
     <div className="flex flex-col items-center text-center py-10 px-4">
       <h1 className="text-3xl font-bold">Restaria</h1>
-      <p className="mt-6 w-120">Restaria Liba biedt niet alleen een uitgebreid assortiment aan heerlijke gerechten voor gasten die graag ter plaatse willen genieten, maar ook een handige afhaalservice voor degenen die liever thuis van onze smaken genieten. Bestel eenvoudig online of telefonisch, en haal je favoriete gerechten op wanneer het jou uitkomt. We kijken ernaar uit om je te verwelkomen in ons restaurant of je bestelling gereed te maken voor afhalen!</p>
+      <p className="mt-6 max-w-120">Restaria Liba biedt niet alleen een uitgebreid assortiment aan heerlijke gerechten voor gasten die graag ter plaatse willen genieten, maar ook een handige afhaalservice voor degenen die liever thuis van onze smaken genieten. Bestel eenvoudig online of telefonisch, en haal je favoriete gerechten op wanneer het jou uitkomt. We kijken ernaar uit om je te verwelkomen in ons restaurant of je bestelling gereed te maken voor afhalen!</p>
       <p className="my-6 text-xl italic">Deze website is momenteel in onderhoud</p>
       <p>Website gemaakt door <a href="https://instagram.com/charleychen18" className="underline text-blue-800">Charley</a></p>
     </div>
