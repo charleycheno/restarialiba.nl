547090e53ff3de20f13805540323fae536f16d31
Charley Chen <charleychen2006@gmail.com>
2024-03-24 00:14:05 +0100

    Fix a minor style bug

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -7,7 +7,7 @@ export default async function Page() {
     <div className="flex flex-col items-center text-center py-10 px-4">
       <h1 className="text-3xl font-bold">Restaria Liba</h1>
       <h2 className="text-xl font-bold">Boven-Hardinxveld</h2>
-      <p className="mt-6 max-max-w-120">Welkom bij Restaria Liba! Ontdek de smaakvolle wereld van heerlijke gerechten die wij te bieden hebben. Van sappige hamburgers tot knapperige frietjes, bij ons vind je alles voor een smakelijke maaltijd. Bekijk ons menu en plaats vandaag nog je bestelling!</p>
+      <p className="mt-6 max-w-120">Welkom bij Restaria Liba! Ontdek de smaakvolle wereld van heerlijke gerechten die wij te bieden hebben. Van sappige hamburgers tot knapperige frietjes, bij ons vind je alles voor een smakelijke maaltijd. Bekijk ons menu en plaats vandaag nog je bestelling!</p>
       <p className="my-6 text-xl italic">Deze website is momenteel in onderhoud</p>
       <p>Website gemaakt door <a href="https://instagram.com/charleychen18" className="underline text-blue-800">Charley</a></p>
     </div>
