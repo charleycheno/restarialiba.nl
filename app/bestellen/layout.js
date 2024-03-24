e215e0640d8b0ebd09bdefd833ded5a634be2966
Charley Chen <charleychen2006@gmail.com>
2024-03-24 23:59:35 +0100

    Update navigation for online ordering

--- app/bestellen/layout.js
+++ app/bestellen/layout.js
@@ -1,3 +1,4 @@
+import OrderAppNav from "@/components/OrderAppNav"
 import Link from "next/link"
 
 export const metadata = {
@@ -14,17 +15,7 @@ export default function Layout({ children }) {
       <main className="flex-1">
         {children}
       </main>
-      <nav className="flex items-center w-full h-20 border-t">
-        <Link className="flex flex-1 items-center justify-center h-full" href="/bestellen">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-8" viewBox="0 0 576 512"><path fill="#2a2a2a" d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>
-        </Link>
-        <Link className="flex flex-1 items-center justify-center h-full" href="/bestellen/winkelmand">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-8" viewBox="0 0 576 512"><path fill="#233588" d="M253.3 35.1c6.1-11.8 1.5-26.3-10.2-32.4s-26.3-1.5-32.4 10.2L117.6 192H32c-17.7 0-32 14.3-32 32s14.3 32 32 32L83.9 463.5C91 492 116.6 512 146 512H430c29.4 0 55-20 62.1-48.5L544 256c17.7 0 32-14.3 32-32s-14.3-32-32-32H458.4L365.3 12.9C359.2 1.2 344.7-3.4 332.9 2.7s-16.3 20.6-10.2 32.4L404.3 192H171.7L253.3 35.1zM192 304v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16s16 7.2 16 16zm96-16c8.8 0 16 7.2 16 16v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16zm128 16v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg>
-        </Link>
-        <Link className="flex flex-1 items-center justify-center h-full" href="/bestellen/favorieten">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-8" viewBox="0 0 512 512"><path fill="#ff0000" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
-        </Link>
-      </nav>
+      <OrderAppNav />
     </div>
   )
 }
