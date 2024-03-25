6ac99d7e1e0fcd47780275722c077448ee8e7d64
Charley Chen <charleychen2006@gmail.com>
2024-03-25 23:39:55 +0100

    Change grid layout and add emojis

--- app/bestellen/page.js
+++ app/bestellen/page.js
@@ -2,37 +2,37 @@ export default function Page() {
   return (
     <div className="container flex flex-col gap-4 mx-auto p-4">
       <div>
-        <h2 className="mb-2 text-xl font-medium">Populair</h2>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="h-24 border-4 rounded-xl"></div>
-          <div className="h-24 border-4 rounded-xl"></div>
-          <div className="h-24 border-4 rounded-xl"></div>
-          <div className="h-24 border-4 rounded-xl"></div>
+        <h2 className="mb-2 text-xl font-medium">Populair 🔥</h2>
+        <div className="grid grid-cols-2 gap-2">
+          <div className="h-28 border-4 rounded-xl"></div>
+          <div className="h-28 border-4 rounded-xl"></div>
+          <div className="h-28 border-4 rounded-xl"></div>
+          <div className="h-28 border-4 rounded-xl"></div>
         </div>
       </div>
       <div>
-        <h2 className="mb-2 text-xl font-medium">Dranken</h2>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="h-24 border-4 rounded-xl"></div>
-          <div className="h-24 border-4 rounded-xl"></div>
-          <div className="h-24 border-4 rounded-xl"></div>
-          <div className="h-24 border-4 rounded-xl"></div>
+        <h2 className="mb-2 text-xl font-medium">Eten 🍴</h2>
+        <div className="grid grid-cols-2 gap-2">
+          <div className="h-28 border-4 rounded-xl"></div>
+          <div className="h-28 border-4 rounded-xl"></div>
+          <div className="h-28 border-4 rounded-xl"></div>
+          <div className="h-28 border-4 rounded-xl"></div>
+          <div className="h-28 border-4 rounded-xl"></div>
+          <div className="h-28 border-4 rounded-xl"></div>
         </div>
       </div>
       <div>
-        <h2 className="mb-2 text-xl font-medium">Eten</h2>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="h-24 border-4 rounded-xl"></div>
-          <div className="h-24 border-4 rounded-xl"></div>
-          <div className="h-24 border-4 rounded-xl"></div>
-          <div className="h-24 border-4 rounded-xl"></div>
-          <div className="h-24 border-4 rounded-xl"></div>
-          <div className="h-24 border-4 rounded-xl"></div>
+        <h2 className="mb-2 text-xl font-medium">Dranken 🥤</h2>
+        <div className="grid grid-cols-2 gap-2">
+          <div className="h-28 border-4 rounded-xl"></div>
+          <div className="h-28 border-4 rounded-xl"></div>
+          <div className="h-28 border-4 rounded-xl"></div>
+          <div className="h-28 border-4 rounded-xl"></div>
         </div>
       </div>
       <div>
         <h2 className="mb-2 text-xl font-medium">Diversen</h2>
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-2">
           <div className="h-24 border-4 rounded-xl"></div>
           <div className="h-24 border-4 rounded-xl"></div>
           <div className="h-32 border-4 rounded-xl col-span-2"></div>
