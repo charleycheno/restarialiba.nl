1b0b80cbc9d048890e142969df5f9d9e8dd8ae7a
Charley Chen <charleychen2006@gmail.com>
2024-03-29 21:01:14 +0100

    Fix minor typo

--- app/(bestellen)/account/page.js
+++ app/(bestellen)/account/page.js
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div className="container flex flex-col items-center text-center gap-4 mx-auto p-4">
-      <p>Maak een een account aan of log in</p>
+      <p>Maak een account aan of log in</p>
     </div>
   )
 }
