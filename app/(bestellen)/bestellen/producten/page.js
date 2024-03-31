090d11361378f27e5b803d5d4072a2bbb8d04f94
Charley Chen <charleychen2006@gmail.com>
2024-03-31 14:34:37 +0200

    Debug test 3

--- app/(bestellen)/bestellen/producten/page.js
+++ app/(bestellen)/bestellen/producten/page.js
@@ -1,22 +1,22 @@
 import AddCard from "@/app/_components/Bestellen/AddCard"
 
 export default async function Page() {
-  const request = await fetch(`https://ljhc.nl/api/get-products`)
-  const data = await request.json()
-  const products = data.products
+  // const request = await fetch(`https://ljhc.nl/api/get-products`)
+  // const data = await request.json()
+  // const products = data.products
   
-  return (
-    <div className="flex flex-col">
-      {
-        products.map((product) => {
-          var prices = []
-          for (let i = 0; i < product.versions.length; i++) {
-            const version = product.versions[i];
-            prices.push(version.price)
-          }
-          return <AddCard name={product.name} key={product._id} prices={prices} />
-        })
-      }
-    </div>
-  )
+  // return (
+  //   <div className="flex flex-col">
+  //     {
+  //       products.map((product) => {
+  //         var prices = []
+  //         for (let i = 0; i < product.versions.length; i++) {
+  //           const version = product.versions[i];
+  //           prices.push(version.price)
+  //         }
+  //         return <AddCard name={product.name} key={product._id} prices={prices} />
+  //       })
+  //     }
+  //   </div>
+  // )
 }
