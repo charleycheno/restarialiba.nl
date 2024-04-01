04bbe2c86d5c554a1835e6a3e4eab112bf539c8b
Charley Chen <charleychen2006@gmail.com>
2024-04-01 17:03:10 +0200

    Change product schema

--- app/_database/models/Product.js
+++ app/_database/models/Product.js
@@ -1,9 +1,29 @@
 const mongoose = require("mongoose")
 
 const productSchema = new mongoose.Schema({
-   name: String,
-   description: String,
-   category: String,
+   productId: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      required: true,
+      unique: true,
+      index: true,
+      dropDups: true
+   },
+   name: {
+      type: String,
+      trim: true,
+      required: true,
+   },
+   description: {
+      type: String,
+      trim: true,
+   },
+   category: {
+      type: String,
+      lowercase: true,
+      trim: true,
+   },
    versions: []
 })
 
