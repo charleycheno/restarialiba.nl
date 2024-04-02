d9356523bf272fab1cff4ac1825fee9127590f8e
Charley Chen <charleychen2006@gmail.com>
2024-04-02 23:30:10 +0200

    Add styles

--- app/_components/Bestellen/VersionForm.js
+++ app/_components/Bestellen/VersionForm.js
@@ -23,11 +23,11 @@ export default function VersionList(props) {
       <>
          {oneVersion && <span className="font-medium text-lg">Prijs: €{versions[0].price.toLocaleString('nl-NL', {minimumFractionDigits: 2})}</span>}
          {!oneVersion && 
-            <div className="flex gap-4">
+            <div className="flex flex-wrap gap-4">
                {
                   versions.map((version) => {
                      return (
-                        <button key={versions.indexOf(version)} className={"flex flex-col items-center justify-center flex-grow border-2 rounded py-4" + (selectedVersion == versions.indexOf(version) ? " bg-zinc-200" : "")} onClick={() => selectVersion(versions.indexOf(version))}>
+                        <button key={versions.indexOf(version)} className={"flex flex-col items-center justify-center flex-grow border-2 rounded p-4" + (selectedVersion == versions.indexOf(version) ? " bg-zinc-200" : "")} onClick={() => selectVersion(versions.indexOf(version))}>
                            <span className="capitalize font-bold">{version.name}</span>
                            <span>€{version.price.toLocaleString('nl-NL', {minimumFractionDigits: 2})}</span>
                         </button>
