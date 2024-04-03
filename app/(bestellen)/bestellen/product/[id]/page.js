29c34fbf795ed16336dc33e73cc183967484f82a
Charley Chen <charleychen2006@gmail.com>
2024-04-03 10:51:28 +0200

    Add product not found error

--- app/(bestellen)/bestellen/product/[id]/page.js
+++ app/(bestellen)/bestellen/product/[id]/page.js
@@ -22,24 +22,30 @@ async function ProductPage(props) {
   const data = await getData(props.params)
   const product = data.product
 
-  return (
-    <div className="flex flex-col gap-4 p-4">
-      <div className="flex gap-4 flex-wrap">
-        <div className="bg-zinc-200 w-16 h-16 rounded-full"></div>
-        <div className="flex flex-col justify-center">
-          <h1 className="text-lg font-medium">{product.name}</h1>
-          <span className="text-sm font-medium text-zinc-600">{product.id}</span>
+  if(product != null) {
+    return (
+      <div className="flex flex-col gap-4 p-4">
+        <div className="flex gap-4 flex-wrap">
+          <div className="bg-zinc-200 w-16 h-16 rounded-full"></div>
+          <div className="flex flex-col justify-center">
+            <h1 className="text-lg font-medium">{product.name}</h1>
+            <span className="text-sm font-medium text-zinc-600">{product.id}</span>
+          </div>
         </div>
+        <div className="flex flex-wrap gap-2">
+          {product.category && <div className="capitalize px-3 py-1 text-sm text-zinc-600 border rounded-full active:bg-zinc-200">{product.category}</div>}
+          <div className="capitalize px-3 py-1 text-sm text-zinc-600 border rounded-full active:bg-zinc-200">[allergie info 1]</div>
+          <div className="capitalize px-3 py-1 text-sm text-zinc-600 border rounded-full active:bg-zinc-200">[allergie info 2]</div>
+        </div>
+        {product.description && <p>{product.description}</p>}
+        <VersionList versions={product.versions} />
       </div>
-      <div className="flex flex-wrap gap-2">
-        {product.category && <div className="capitalize px-3 py-1 text-sm text-zinc-600 border rounded-full active:bg-zinc-200">{product.category}</div>}
-        <div className="capitalize px-3 py-1 text-sm text-zinc-600 border rounded-full active:bg-zinc-200">[allergie info 1]</div>
-        <div className="capitalize px-3 py-1 text-sm text-zinc-600 border rounded-full active:bg-zinc-200">[allergie info 2]</div>
-      </div>
-      {product.description && <p>{product.description}</p>}
-      <VersionList versions={product.versions} />
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className="m-4 text-center">Product niet gevonden</div>
+    )
+  }
 }
 
 export default async function Page({ params }) {
