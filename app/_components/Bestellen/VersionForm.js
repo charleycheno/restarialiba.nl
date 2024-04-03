0bb883d50d97be725bfe441ab3975b69e72afbbc
Charley Chen <charleychen2006@gmail.com>
2024-04-03 23:42:12 +0200

    Animate add button

--- app/_components/Bestellen/VersionForm.js
+++ app/_components/Bestellen/VersionForm.js
@@ -1,6 +1,5 @@
 "use client"
 
-import { isBot } from "next/dist/server/web/spec-extension/user-agent"
 import { useState } from "react"
 
 export default function VersionList(props) {
@@ -90,7 +89,7 @@ export default function VersionList(props) {
                }
             </div>
          }
-         <button className="flex items-center justify-center gap-1 bg-blue-400 rounded-full py-3 text-white text-lg font-bold" onClick={addHandler}><svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#fff" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>Toevoegen</button>
+         <button className="flex items-center justify-center gap-1 bg-blue-400 rounded-full py-3 text-white text-lg font-bold active:scale-95 transition" onClick={addHandler}><svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#fff" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>Toevoegen</button>
       </>
    )
 }
