34a78d524c778c776fb632715c700f0204ceecb8
Charley Chen <charleychen2006@gmail.com>
2024-04-04 22:48:32 +0200

    Add cart items from localstorage to cart page

--- app/(bestellen)/winkelmand/page.js
+++ app/(bestellen)/winkelmand/page.js
@@ -1,7 +1,15 @@
+"use client"
+
 export default function Page() {
+  const cart = JSON.parse(localStorage.getItem("cart"))
+
   return (
     <div className="container flex flex-col items-center text-center gap-4 mx-auto p-4">
-      <span>De winkelmand is leeg</span>
+      {cart == null ? <span>De winkelmand is leeg</span> :
+        cart.map((item) => {
+          return <span>{item.quantity}x {item.id}</span>
+        })
+      }
     </div>
   )
 }
