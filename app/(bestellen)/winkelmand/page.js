ef6f10beec759a31adaddb8e2648fef405022e93
Charley Chen <charleychen2006@gmail.com>
2024-04-05 23:24:32 +0200

    Fix bug on cart page

--- app/(bestellen)/winkelmand/page.js
+++ app/(bestellen)/winkelmand/page.js
@@ -1,11 +1,17 @@
 "use client"
 
+import { useEffect, useState } from "react"
+
 export default function Page() {
-  const cart = JSON.parse(localStorage.getItem("cart"))
+  const [cart, setCart] = useState([])
+
+  useEffect(() => {
+    setCart(JSON.parse(localStorage.getItem("cart")))
+  }, [])
 
   return (
     <div className="container flex flex-col items-center text-center gap-4 mx-auto p-4">
-      {cart == null ? <span>De winkelmand is leeg</span> :
+      {cart == null || cart.length == 0 ? <span>De winkelmand is leeg</span> :
         cart.map((item) => {
           return <span>{item.quantity}x {item.id}</span>
         })
