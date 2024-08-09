22dfcd22874facbdf0a181fbba78fda622cbe57f
Charley <charleychen2006@gmail.com>
2024-08-09 13:49:44 +0200

    Add password reset prompt

--- app/(auth)/nieuw-wachtwoord/page.js
+++ app/(auth)/nieuw-wachtwoord/page.js
@@ -13,6 +13,17 @@ export default function Page() {
 
    const { data, error } = supabase.auth.getUser()
 
+   supabase.auth.onAuthStateChange(async (event, session) => {
+      if (event == "PASSWORD_RECOVERY") {
+         const newPassword = prompt("What would you like your new password to be?");
+         const { data, error } = await supabase.auth
+            .updateUser({ password: newPassword })
+
+         if (data) alert("Password updated successfully!")
+         if (error) alert("There was an error updating your password.")
+      }
+   })
+
    if(data?.user) {
       return (
          <div className="flex justify-center p-4 md:py-10">
